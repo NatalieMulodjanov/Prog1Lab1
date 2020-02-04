0e872871ed8fc96809a5cfd16203638a62cd0e83
Natalie Mulodjanov <ntaliemulodjanov@gmail.com>
2020-02-04 11:14:11 -0500

    calculate the perimeter and area

---
+++ RectanglePerimeter.java
@@ -0,0 +1,19 @@
+public class RectanglePerimeter {
+  public static void main (String[] args){
+
+    double length;
+    double windth;
+    double perimeter;
+    double area;
+    length = 5;
+    windth = 5;
+
+    perimeter = 2 * (length + windth);
+    area = length * windth;
+
+    System.out.println(" rectangle perimeter  " + perimeter +
+                        "\nrectangle area " + area);
+
+
+  }
+}
