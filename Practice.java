833aff5a5c2f53ae1f5b68a0b6d4a33a11fc27e3
Natalie Mulodjanov <ntaliemulodjanov@gmail.com>
2020-02-06 11:47:55 -0500

    practice from slides

---
+++ Practice.java
@@ -0,0 +1,12 @@
+public class Practice {
+  public static void main (String [] args){
+
+    String word1 =  "Hello";
+    String word2 =  "there";
+    String word3 =  "world";
+
+    String greeting = word1 + word2 + word3;
+
+    System.out.println(greeting);
+  }
+}
