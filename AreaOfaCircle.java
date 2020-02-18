b0ea66840bd86368bb024b6d833837be0ed9b96e
cstuser <ntaliemulodjanov@gmail.com>
2020-02-18 09:00:13 -0500

    added a scanner object so the user can input their own radius

--- AreaOfaCircle.java
+++ AreaOfaCircle.java
@@ -1,11 +1,26 @@
-public class AreaOfaCircle {
+//importing java utility package 
+import java.util.*;
 
+public class AreaOfaCircle {
   public static void main(String[] args) {
+
+  	//instantiating objects and declaring variables 
     final double PI = 3.14159;
     double radius;
     double area;
-    radius = 3.5;
+    Scanner sc = new Scanner (System.in);
+
+    //header 
+    System.out.println("******AREA OF A CIRCLE CALCULATPOR");
+    
+    //getting radius from user 
+    System.out.println("Please enter the Radius");
+	radius = sc.nextDouble();
+
+	//math and calculations 
     area = PI * radius * radius;
+    
+    //output 
     System.out.println ("The area is " + area );
 
   }
